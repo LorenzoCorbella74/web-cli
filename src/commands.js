8e8eb1a93589446fa149ffe0fce2bd3b05f6a20d
Lorenzo Corbella <ing.lorenzo.corbella@g,ail.com>
2020-05-01 19:08:01 +0200

    fix temp

--- src/commands.js
+++ src/commands.js
@@ -172,7 +172,7 @@ export const commands = {
                     .then(data => {
                         instance.loader(false);
                         console.log(data);
-                        instance.writeHTML((` <p> > :meteo  for ${city}: ${data.weather[0].description} - ${data.main[0].temp} - <img class="icon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather icon"></p>`));
+                        instance.writeHTML((` <p> > :meteo  for ${city}: ${data.weather[0].description} - ${data.main.temp} - <img class="icon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather icon"></p>`));
                     })
                     .catch(error => {
                         console.error(error);
